df25a76aed95cf7fa30b5f49f1d73daea2a22f6b
Seraphim McGann <seraphim.mcgann@yale.edu>
2026-09-14 17:38:56 -0400

    Adding ocs-web to docker-compose, pointing alphadm root at homer, and adding more tiles to homer

--- prepare-domains.sh
+++ prepare-domains.sh
@@ -3,4 +3,7 @@ cat >> /etc/hosts <<EOF
 127.0.0.1       alphadm.wlab.yale.edu
 127.0.0.1       whoami.alphadm.wlab.yale.edu
 127.0.0.1       auth.alphadm.wlab.yale.edu
+127.0.0.1       grafana.alphadm.wlab.yale.edu
+127.0.0.1       ocs-web.alphadm.wlab.yale.edu
+127.0.0.1       lldap.alphadm.wlab.yale.edu
 EOF
